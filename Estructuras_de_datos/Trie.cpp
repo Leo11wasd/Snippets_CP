f2e3c0efa532852b06a8793f358171274882e4da
Leopoldo Morante <98835751+Leo11wasd@users.noreply.github.com>
2025-02-11 16:34:12 -0600

    nueva version

--- Estructuras_de_datos/Trie.cpp
+++ Estructuras_de_datos/Trie.cpp
@@ -1,50 +1,68 @@
-struct TrieNode{
-    map<char,TrieNode*> children;
+struct TrieNode
+{
+    map<char, TrieNode *> children;
     bool flag = false;
-    ll cont = 1;
-    void setEnd(){
+    ll cont = 0;
+    void setEnd()
+    {
         flag = true;
     }
-    void sumCont(){
-      cont = cont+1;
+    void sumCont()
+    {
+        cont = cont + 1;
     }
-    void resCont(){
-      cont = cont-1;
+    void resCont()
+    {
+        cont = cont - 1;
     }
-    bool isEnd(){
+    bool isEnd()
+    {
         return flag;
     }
 };
-void insert(TrieNode* root, const string& word){
-    TrieNode* node = root;
-    for(int i=0;i<word.length();++i){
-        if(!node->children.count(word[i])){
-          node->children[word[i]] = new TrieNode();
+void insert(TrieNode *root, const string &word)
+{
+    TrieNode *node = root;
+    for (int i = 0; i < word.length(); ++i)
+    {
+        if (!node->children.count(word[i]))
+        {
+            node->children[word[i]] = new TrieNode();
         }
+        node->children[word[i]]->sumCont();
         node = node->children[word[i]];
     }
     node->setEnd();
 }
 
-ll search(TrieNode* root,string& word){
-    TrieNode* node = root;
-    for(int i=0;i<word.length();++i){
-if(!node->children.count(word[i])){
+ll search(TrieNode *root, string &word)
+{
+    TrieNode *node = root;
+    for (int i = 0; i < word.length(); ++i)
+    {
+        if (!node->children.count(word[i]))
+        {
             return 0;
         }
         node = node->children[word[i]];
     }
-    if(node->isEnd()){
-      return node->cont;
-    }else{
-      return 0;
+    if (node->isEnd())
+    {
+        return node->cont;
+    }
+    else
+    {
+        return 0;
     }
 }
 
-bool startsWith(TrieNode* root,string& prefix){
-    TrieNode* node = root;
-    for(int i=0;i<prefix.length();++i){
-        if(!node->children.count(prefix[i])){
+bool startsWith(TrieNode *root, string &prefix)
+{
+    TrieNode *node = root;
+    for (int i = 0; i < prefix.length(); ++i)
+    {
+        if (!node->children.count(prefix[i]))
+        {
             return false;
         }
         node = node->children[prefix[i]];
